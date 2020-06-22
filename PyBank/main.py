93ed3a3bcbf8255334c3e6b5cedc96c3f3fdcfda
OlegRyzhkov2020 <61919810+OlegRyzhkov2020@users.noreply.github.com>
2020-06-21 21:50:28 -0500

    PyBank challenge analysis and printing

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,12 @@ import os
 import csv
 from datetime import datetime
 
+# Description of output messages
+MSG_1 = "\nFinacial Analysis\n{}\nTotal Months:{}\nTotal:{}"
+MSG_2 = "\nAverage Change: ${}"
+MSG_3 = "Greatest Increase in Profits (${})  Date(YY-MM):{}-{}"
+MSG_4 = "Greatest Decrease in Profits (${}) Date(YY-MM):{}-{}\n{}"
+
 def load_data(path, file_name):
     """Load a data set.
     Args:
@@ -15,9 +21,9 @@ def load_data(path, file_name):
     # Opening CSV file
     with open(data_csv, 'r') as csvfile:
         data_reader = csv.reader(csvfile, delimiter=',')
-    # Removing header
+        # Removing header
         next(data_reader, None)
-    # Convert CSV file into a nested list
+        # Convert CSV file into a nested list
         data_list = []
         for row in data_reader:
             data_list.append(row)
@@ -25,25 +31,67 @@ def load_data(path, file_name):
     return data_list
 
 def conversion_data(data_list):
-    """Converts string data to data and integer types,
-    adds column with the changes of "Profit/Losses".
+    """Converts string data to date and integer types,
+    adds list element with the changes of "Profit/Losses".
     Args:
         data_list (list): data list for conversion.
     Returns:
-        conversion_list: (updated list after conversion)
+        conversion_dict:
+            key - list (type date) with 1 element date
+            values - list (type integer) with 2 elements (profit, change)
     """
-
+    # Conversion string into data type
     date_list = [datetime.strptime(data_str[0], '%b-%Y') for data_str in data_list]
+    # Conversion string into integer type
     profit_list = [int(data_str[1]) for data_str in data_list]
-    conversion_list = dict(zip(date_list, profit_list))
+    # Merging 2 lists into a dictionary
+    conversion_dict = dict(zip(date_list, profit_list))
+    # Appending changes element to a value for each key of a dictionary
+    changes_list = []
+    for i in range(len(profit_list)):
+        if i == 0 : changes_list.append(0)
+        else:
+            changes_list.append(profit_list[i] - profit_list[i-1])
+        conversion_dict[date_list[i]] = [conversion_dict[date_list[i]], changes_list[i]]
+    # Returning dictionary with conversed and added data
+    return conversion_dict
 
-    return conversion_list
+def print_plus(print_list):
+    """Printing data to the terminal.
+    Args:
+        data_list (tuple): data list for printing.
+    """
+    #Printing the analysis result to the terminal
+    border = '#' * 60
+    print(MSG_1.format(border, print_list[0], print_list[1]))
+    print(MSG_2.format(print_list[2]))
+    print(MSG_3.format(print_list[3], print_list[4].year, print_list[4].month))
+    print(MSG_4.format(print_list[5], print_list[6].year, print_list[6].month, border))
 
 #Call function load_data for PyBank
 pybank_data = load_data("Resources", "budget_data.csv")
-
-border = '#' * 50
-# print('{}\n{}'.format(border, pybank_data))
-
+#Call function conversion_data for PyBank uploaded data into paybank_data
 pybank_conversion = conversion_data(pybank_data)
-print('{}\n{}'.format(border, pybank_conversion))
+
+#Dictionary records analysis
+total_profit = 0
+total_change = 0
+max_change = 0
+min_change = 0
+for key in pybank_conversion:
+    total_profit += pybank_conversion[key][0]
+    total_change += pybank_conversion[key][1]
+    if pybank_conversion[key][1] > max_change:
+        max_change = pybank_conversion[key][1]
+        max_date = key
+    if pybank_conversion[key][1] < min_change:
+        min_change = pybank_conversion[key][1]
+        min_date = key
+total_months = len(pybank_conversion)
+avg_change = round(total_change/total_months,2)
+
+#Creating tuple for output data
+print_data = (total_months, total_profit, avg_change, max_change, max_date, min_change, min_date)
+
+#Call function print_plus
+print_plus(print_data)
